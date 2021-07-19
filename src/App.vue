2bb16874b628739468f259d0eaee73732c59f76a
GuilhermeReckziegel <guilhermealexandre50@gmail.com>
2021-07-19 13:13:11 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/logo1.jpg" alt="Logo">
+      <br>
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Novo To-Do">
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+          <div class="tile flex-centered" v-for="t in todos" :key="t.id">
+            <div class="tile-icon">
+                <i class="icon icon-time"></i>
+            </div>
+            <div class="tile-content">
+              <div class="tile-subtitle"> {{ t.description }} </div>
+            </div>
+            <div class="tile-action">
+              <button class="btn btn-link">Concluído</button>
+              <button class="btn btn-link">
+                <span class="text-error">Remover</span>
+              </button>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return { todos: [], todo: { checked: false } };
+  },
+
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false };
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo{
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.todo-list {
+  padding-top: 2rem;
 }
 </style>
